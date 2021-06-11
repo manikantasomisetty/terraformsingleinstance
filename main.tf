d83b907fa86f0f31546066f0de0a257161f12090
manikantasomisetty <84460458+manikantasomisetty@users.noreply.github.com>
2021-06-11 16:49:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,12 @@ provider "aws" {
 	
     region = "${var.aws_region}"
 }
-
 terraform {
-  required_version = "<= 0.14" #Forcing which version of Terraform needs to be used
-  required_providers {
-    aws = {
-      version = "<= 3.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
-    }
-  }
+	backend "s3" {
+		bucket = "manibucket123"
+		key = "jenkins.tfstate"
+		region = "us-east-1"
+	}
 }
 
 resource "aws_vpc" "default" {
